7c4031d271283845f185eb7e01bb6f3c28a85b12
SuchetBhosale <suchetbhosale77@gmail.com>
2024-02-05 04:29:02 +0530

    nav bar and session done

---
+++ Data/data.js
@@ -0,0 +1,47 @@
+const items = [
+    {
+        id: '1',
+        Image:'/FlipkartClone/Images/sessionImageGrocery.png',
+        label:'Grocery',
+    },
+    {
+        id: '2',
+        Image:'/FlipkartClone/Images/sessionImageMobilepng.png',
+        label:'Mobiles',
+    },
+    {
+        id: '3',
+        Image:'/FlipkartClone/Images/sessionImageFashion.png',
+        label:'Fashions',
+    },
+    {
+        id: '4',
+        Image:'/FlipkartClone/Images/sessionImageElectronicspng.png',
+        label:'Electronics',
+    },
+    {
+        id: '5',
+        Image:'/FlipkartClone/Images/sessionImageHomeFurniture.png',
+        label:'Home and Furniture',
+    },
+    {
+        id: '6',
+        Image:'/FlipkartClone/Images/sessionImageAppliances.png',
+        label:'Appliances',
+    },
+    {
+        id: '7',
+        Image:'/FlipkartClone/Images/sessionImageTravel.png',
+        label:'Travel',
+    },
+    {
+        id: '8',
+        Image:'/FlipkartClone/Images/sessionImageToys.png',
+        label:'Beauty, Toys & More',
+    },
+    {
+        id: '9',
+        Image:'/FlipkartClone/Images/sessionImageTwoWheeler.png',
+        label:'Two Wheeler',
+    },
+]
---
+++ javascript/index.js
@@ -0,0 +1,9 @@
+// let sessions = document.querySelector(".sessions-container")
+
+// let innerHTML= ``;
+// items.forEach(items => {
+//     innerHTML = innerHTML + `
+//     `
+// })
+
+// sessions.innerHTML= innerHTML;
